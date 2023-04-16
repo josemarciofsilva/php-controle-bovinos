0d0e5f734960d65dfd7bd9dbeb2d7e0bf42faf0f
Jose Silva <t.jose.silva@bca-br.local>
2023-04-16 11:46:32 -0300

    Adicionado dois metodos na classe CreateBovino, metodo execute e checkDateOfBirthFuture, este último faz a verificação se a data é maior que o dia atual, caso contrário lança uma excessão

--- src/Application/CreateBovino.php
+++ src/Application/CreateBovino.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace App\Application;
 
+use App\Domain\Bovino;
+use App\Domain\BovinoException;
+use DateTime;
+use Ramsey\Uuid\Uuid;
+
+
 class CreateBovino
 {
     public function __construct()
@@ -13,6 +19,29 @@ class CreateBovino
 
     public function execute(array $data): array
     {
-        return $data;
+        $uuid = Uuid::uuid4()->toString();
+
+        $dateOfBirth = $data['dateOfBirth'];
+        $this->checkDateOfBirthFuture($dateOfBirth);
+
+        $bovino = new Bovino(
+            $uuid,
+            $data['race'],
+            $data['weight'],
+            new DateTime($data['dateOfBirth']),
+            $data['productionOfLitersOfMilkPerWeek'],
+            new DateTime(),
+            new DateTime()
+        );
+
+       return $bovino->toArray();
+    }
+
+    private function checkDateOfBirthFuture(string $dateOfBirth): void
+    {
+       $hoje = date('Y-m-d');
+       if(strtotime($dateOfBirth) > strtotime($hoje)){
+            throw BovinoException::forInvalidDateOfBirth();
+       }
     }
 }
